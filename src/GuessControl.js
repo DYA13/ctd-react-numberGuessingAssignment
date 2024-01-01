0ab7b5f265a7642efc5ade5c582a7a859d3cb0f2
Yelena Japarova <yelena.dzhaparova@gmail.com>
2024-01-01 18:21:49 +0100

    Convert GuessControl

--- src/GuessControl.js
+++ src/GuessControl.js
@@ -1,47 +1,28 @@
-import React, { Component } from "react";
-import Button from "./Button";
+import React, { Component } from "react"
+import Button from "./Button"
 
-class GuessControl extends Component {
-  constructor(props) {
-    super(props);
+const GuessControl = ({ onGuess }) => {
+  const [currentGuess, setCurrentGuess] = useState("")
 
-    this.state = {
-      currentGuess: "",
-    };
-
-    /**
-     * These lines are required to make the methods/functions declared on this
-     *  class have the correct `this` object when they run.
-     */
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.onSubmitGuess = this.onSubmitGuess.bind(this);
+  const handleInputChange = (event) => {
+    setCurrentGuess(event.target.value)
   }
 
-  handleInputChange(event) {
-    this.setState({ currentGuess: event.target.value });
+  const onSubmitGuess = () => {
+    // convert the input value to a number before passing it to the onGuess prop.
+    onGuess(Number(currentGuess))
+
+    // clear the currentGuess after submitting.
+    setCurrentGuess("")
   }
 
-  onSubmitGuess() {
-    // Since the values from an HTML input are strings by default,
-    //  convert to a number for the returned guess value
-    //  by passing in the string to the Number function.
-    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-    this.props.onGuess(Number(this.state.currentGuess));
-    this.setState({ currentGuess: "" });
-  }
+  return (
+    <div>
+      <input type='number' value={currentGuess} onChange={handleInputChange} />
 
-  render() {
-    return (
-      <div>
-        <input
-          type="number"
-          value={this.state.currentGuess}
-          onChange={this.handleInputChange}
-        />
-        <Button onClick={this.onSubmitGuess}>Submit Guess</Button>
-      </div>
-    );
-  }
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  )
 }
 
-export default GuessControl;
+export default GuessControl
